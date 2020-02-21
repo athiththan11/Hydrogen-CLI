8c8c7d381c959bebcd804aaabe9c37ade70e6709
athiththan11 <code19111995@gmail.com>
2020-02-21 12:45:47 +0530

    distributed implementation

--- src/commands/distribute/apim.js
+++ src/commands/distribute/apim.js
@@ -18,155 +18,197 @@ class DistributeAPIMCommand extends Command {
 		const generate = flags.generate;
 		const version = flags.version;
 
-		// config parser
-		let config = {};
-		if (flags.config) {
-			config = await Utils.Parser.configParser(require('path').join(process.cwd(), flags.config));
-		}
+		try {
+			// config parser
+			let config = {};
+			if (flags.config) {
+				config = await Utils.Parser.configParser(require('path').join(process.cwd(), flags.config));
+			}
 
-		if (
-			flags[ConfigMaps.Hydrogen.layout.apim.publishMultipleGateway] ||
-			flags[ConfigMaps.Hydrogen.layout.apim.iskm] ||
-			flags[ConfigMaps.Hydrogen.layout.apim.distributed]
-		) {
-			this.log(`Starting to configure WSO2 API Manager ${version}`);
-			// publish-multiple-gateway block
-			if (flags[ConfigMaps.Hydrogen.layout.apim.publishMultipleGateway]) {
-				// set sample model configurations
-				let environmentConfs = Samples.Models.environmentConfs;
-				let layoutConfs = Samples.Models.layoutConfs;
+			if (
+				flags[ConfigMaps.Hydrogen.layout.apim.publishMultipleGateway] ||
+				flags[ConfigMaps.Hydrogen.layout.apim.iskm] ||
+				flags[ConfigMaps.Hydrogen.layout.apim.distributed]
+			) {
+				this.log(`Starting to configure WSO2 API Manager ${version}`);
+				// publish-multiple-gateway block
+				if (flags[ConfigMaps.Hydrogen.layout.apim.publishMultipleGateway]) {
+					// set sample model configurations
+					let environmentConfs = Samples.Models.environmentConfs;
+					let layoutConfs = Samples.Models.layoutConfs;
 
-				if (flags.config && config != null) {
-					// validate the parsed configuration against the schema model
-					let valid = await Utils.Parser.validateConfigs(
-						Schemas.LayoutConfs.publishMultipleGatewaySchema,
-						config
-					);
+					if (flags.config && config != null) {
+						// validate the parsed configuration against the schema model
+						let valid = await Utils.Parser.validateConfigs(
+							Schemas.LayoutConfs.publishMultipleGatewaySchema,
+							config
+						);
 
-					// if validation fails stop the process
-					if (!valid.valid) {
-						return valid.message == null
-							? this.log('Given configuration is not complying with the expected schema')
-							: this.log(valid.message);
+						// if validation fails stop the process
+						if (!valid.valid) {
+							return valid.message == null
+								? this.log('Given configuration is not complying with the expected schema')
+								: this.log(valid.message);
+						}
+
+						environmentConfs = config.environmentConfs;
+						layoutConfs = config.layoutConfs;
 					}
 
-					environmentConfs = config.environmentConfs;
-					layoutConfs = config.layoutConfs;
+					if (flags.count >= 2) {
+						this.log(
+							`Deployment setup "Publishing Through Multiple Gateway" with Gateway Nodes : ${flags.count}`
+						);
+
+						await ExecutionPlans.Deployment.configurePublishMultipleGateway(
+							process.cwd(),
+							flags.count,
+							layoutConfs,
+							environmentConfs
+						);
+						await Utils.Docs.generatePublishMultipleGatewayDocs(flags.count, layoutConfs);
+					} else {
+						this.log('\n' + 'ERROR :: Number of Gateway nodes should be 2 (default) or more' + '\n');
+						this._help();
+					}
 				}
+				// is-km block
+				if (flags[ConfigMaps.Hydrogen.layout.apim.iskm]) {
+					// set sample model configurations
+					let apimlayoutConfs = Samples.Models.apimlayoutConfs;
+					let iskmlayoutConfs = Samples.Models.iskmlayoutConfs;
 
-				if (flags.count >= 2) {
-					this.log(
-						`Deployment setup "Publishing Through Multiple Gateway" with Gateway Nodes : ${flags.count}`
-					);
+					if (flags.config && config != null) {
+						// validate the parsed configuration
+						let valid = await Utils.Parser.validateConfigs(Schemas.LayoutConfs.iskmSchema, config);
 
-					await ExecutionPlans.Deployment.configurePublishMultipleGateway(
-						process.cwd(),
-						flags.count,
-						layoutConfs,
-						environmentConfs
-					);
-					await Utils.Docs.generatePublishMultipleGatewayDocs(flags.count, layoutConfs);
-				} else {
-					this.log('\n' + 'ERROR :: Number of Gateway nodes should be 2 (default) or more' + '\n');
-					this._help();
-				}
-			}
-			// is-km block
-			if (flags[ConfigMaps.Hydrogen.layout.apim.iskm]) {
-				// set sample model configurations
-				let apimlayoutConfs = Samples.Models.apimlayoutConfs;
-				let iskmlayoutConfs = Samples.Models.iskmlayoutConfs;
+						// if validation fails stop the process
+						if (!valid.valid) {
+							return valid.message == null
+								? this.log('Given configuration is not complying with the expected schema')
+								: this.log(valid.message);
+						}
 
-				if (flags.config && config != null) {
-					// validate the parsed configuration
-					let valid = await Utils.Parser.validateConfigs(Schemas.LayoutConfs.iskmSchema, config);
-
-					// if validation fails stop the process
-					if (!valid.valid) {
-						return valid.message == null
-							? this.log('Given configuration is not complying with the expected schema')
-							: this.log(valid.message);
+						apimlayoutConfs = config.apimlayoutConfs;
+						iskmlayoutConfs = config.iskmlayoutConfs;
 					}
 
-					apimlayoutConfs = config.apimlayoutConfs;
-					iskmlayoutConfs = config.iskmlayoutConfs;
+					// retrieve datasource configurations
+					let datasourceConfs = null;
+					if (datasource === ConfigMaps.Hydrogen.datasource.mysql)
+						datasourceConfs = await DatasourceConfigs.MySQL.getDatasourceConfigs(
+							ConfigMaps.Hydrogen.platform.apim,
+							{ setup: true }
+						);
+					if (datasource === ConfigMaps.Hydrogen.datasource.mssql)
+						datasourceConfs = await DatasourceConfigs.MSSQL.getDatasourceConfigs(
+							ConfigMaps.Hydrogen.platform.apim,
+							{ setup: true }
+						);
+					if (datasource === ConfigMaps.Hydrogen.datasource.postgre)
+						datasourceConfs = await DatasourceConfigs.Postgre.getDatasourceConfigs(
+							ConfigMaps.Hydrogen.platform.apim,
+							{ setup: true }
+						);
+
+					if (datasourceConfs != null) {
+						this.log(`Deployment setup "Identity Server as Key Manager"`);
+
+						await ExecutionPlans.Deployment.configureIdentityServerasKeyManager(
+							process.cwd(),
+							datasourceConfs,
+							apimlayoutConfs,
+							iskmlayoutConfs
+						);
+						await Utils.Docs.generateIdentityServerasKMDocs(iskmlayoutConfs);
+
+						// container configurations
+						// mysql container
+						if (datasource === ConfigMaps.Hydrogen.datasource.mysql && container)
+							await Docker.MySQL.createMySQLDockerContainer(
+								ConfigMaps.Hydrogen.platform.apim,
+								{ setup: true, 'is-km': true, generate },
+								process.cwd()
+							);
+						// postgre container
+						if (datasource === ConfigMaps.Hydrogen.datasource.postgre && container)
+							await Docker.Postgre.createPostgreDockerContainer(
+								ConfigMaps.Hydrogen.platform.apim,
+								{ setup: true, 'is-km': true, generate },
+								process.cwd()
+							);
+						// mmsql container
+						if (datasource === ConfigMaps.Hydrogen.datasource.mssql && container)
+							await Docker.MSSQL.createMSSQLDockerContainer(
+								ConfigMaps.Hydrogen.platform.apim,
+								{ setup: true, 'is-km': true, generate },
+								process.cwd()
+							);
+					}
 				}
+				// distributed block
+				if (flags[ConfigMaps.Hydrogen.layout.apim.distributed]) {
+					// set sample model configurations
+					let tmlayoutConfs = Samples.Models.tmlayoutConfs;
+					let storelayoutConfs = Samples.Models.storelayoutConfs;
+					let publisherlayoutConfs = Samples.Models.publisherlayoutConfs;
+					let kmlayoutConfs = Samples.Models.kmlayoutConfs;
+					let gatewaylayoutConfs = Samples.Models.gatewaylayoutConfs;
 
-				// retrieve datasource configurations
-				let datasourceConfs = null;
-				if (datasource === ConfigMaps.Hydrogen.datasource.mysql)
-					datasourceConfs = await DatasourceConfigs.MySQL.getDatasourceConfigs(
-						ConfigMaps.Hydrogen.platform.apim,
-						{ setup: true }
-					);
-				if (datasource === ConfigMaps.Hydrogen.datasource.mssql)
-					datasourceConfs = await DatasourceConfigs.MSSQL.getDatasourceConfigs(
-						ConfigMaps.Hydrogen.platform.apim,
-						{ setup: true }
-					);
-				if (datasource === ConfigMaps.Hydrogen.datasource.postgre)
-					datasourceConfs = await DatasourceConfigs.Postgre.getDatasourceConfigs(
-						ConfigMaps.Hydrogen.platform.apim,
-						{ setup: true }
-					);
+					if (flags.config && config != null) {
+						// validate the parsed configuration
+						let valid = await Utils.Parser.validateConfigs(Schemas.LayoutConfs.iskmSchema, config);
 
-				if (datasourceConfs != null) {
-					this.log(`Deployment setup "Identity Server as Key Manager"`);
+						// if validation fails stop the process
+						if (!valid.valid) {
+							return valid.message == null
+								? this.log('Given configuration is not complying with the expected schema')
+								: this.log(valid.message);
+						}
 
-					await ExecutionPlans.Deployment.configureIdentityServerasKeyManager(
-						process.cwd(),
-						datasourceConfs,
-						apimlayoutConfs,
-						iskmlayoutConfs
-					);
-					await Utils.Docs.generateIdentityServerasKMDocs(iskmlayoutConfs);
+						tmlayoutConfs = config.tmlayoutConfs;
+						storelayoutConfs = config.storelayoutConfs;
+						publisherlayoutConfs = config.publisherlayoutConfs;
+						kmlayoutConfs = config.kmlayoutConfs;
+						gatewaylayoutConfs = config.gatewaylayoutConfs;
+					}
 
-					// container configurations
-					// mysql container
-					if (datasource === ConfigMaps.Hydrogen.datasource.mysql && container)
-						await Docker.MySQL.createMySQLDockerContainer(
+					// retrieve datasource configurations
+					let datasourceConfs = null;
+					if (datasource === ConfigMaps.Hydrogen.datasource.mysql)
+						datasourceConfs = await DatasourceConfigs.MySQL.getDatasourceConfigs(
 							ConfigMaps.Hydrogen.platform.apim,
-							{ setup: true, 'is-km': true, generate },
-							process.cwd()
+							{ setup: true }
 						);
-					// postgre container
-					if (datasource === ConfigMaps.Hydrogen.datasource.postgre && container)
-						await Docker.Postgre.createPostgreDockerContainer(
+					if (datasource === ConfigMaps.Hydrogen.datasource.mssql)
+						datasourceConfs = await DatasourceConfigs.MSSQL.getDatasourceConfigs(
 							ConfigMaps.Hydrogen.platform.apim,
-							{ setup: true, 'is-km': true, generate },
-							process.cwd()
+							{ setup: true }
 						);
-					// mmsql container
-					if (datasource === ConfigMaps.Hydrogen.datasource.mssql && container)
-						await Docker.MSSQL.createMSSQLDockerContainer(
+					if (datasource === ConfigMaps.Hydrogen.datasource.postgre)
+						datasourceConfs = await DatasourceConfigs.Postgre.getDatasourceConfigs(
 							ConfigMaps.Hydrogen.platform.apim,
-							{ setup: true, 'is-km': true, generate },
-							process.cwd()
+							{ setup: true }
 						);
+
+					if (datasourceConfs != null) {
+						this.log(`Deployment setup "Distributed"`);
+
+						await ExecutionPlans.Deployment.configureDistributedDeployment(process.cwd(), datasourceConfs, {
+							kmlayoutConfs,
+							publisherlayoutConfs,
+							storelayoutConfs,
+							tmlayoutConfs,
+							gatewaylayoutConfs
+						});
+						// TODO: generate docs
+						// TODO: container generation
+					}
 				}
 			}
-			// distributed block
-			if (flags[ConfigMaps.Hydrogen.layout.apim.distributed]) {
-				// set sample model configurations
-				let tmlayoutConfs = Samples.Models.tmlayoutConfs;
-				let storelayoutConfs = Samples.Models.storelayoutConfs;
-				let publisherlayoutConfs = Samples.Models.publisherlayoutConfs;
-
-				// TODO: read config and parse
-
-				// TODO: datasource configurations | FIXME:
-				let datasourceConfs = await DatasourceConfigs.Postgre.getDatasourceConfigs(
-					ConfigMaps.Hydrogen.platform.apim,
-					{ setup: true }
-				);
-
-				// TODO: execute distributed execution plan
-				await ExecutionPlans.Deployment.configureDistributedDeployment(process.cwd(), datasourceConfs, {
-					publisherlayoutConfs,
-					storelayoutConfs,
-					tmlayoutConfs
-				});
-			}
+		} catch (err) {
+			this.log(err);
+			this.error(err, { exit: true });
 		}
 	}
 }
